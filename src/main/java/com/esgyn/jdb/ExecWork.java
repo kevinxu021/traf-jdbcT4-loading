53db1a125d7e4dccddd2042936500baee0e131ec
Kevin Xu <kaihua.xu@esgyn.cn>
2015-11-13 11:32:38 +0800

    support perticular charactors

--- src/main/java/com/esgyn/jdb/ExecWork.java
+++ src/main/java/com/esgyn/jdb/ExecWork.java
@@ -1,20 +1,14 @@
 package com.esgyn.jdb;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.zaxxer.hikari.HikariDataSource;
 
 public class ExecWork implements Runnable {
 	private static final Logger log = LoggerFactory.getLogger(ExecWork.class);
@@ -28,12 +22,14 @@ public class ExecWork implements Runnable {
 	private static long cnt = 0;
 	private final String newline = System.getProperty("line.separator");
 	private NullString ns = new NullString();
+	private int separator;
 
 	public ExecWork(Properties conf) throws Exception {
 		this.conf = conf;
 		this.threads = Integer.parseInt(conf.getProperty("tgz_threads", "3"));
 		this.batchSize = Integer.parseInt(conf.getProperty("tgz_batch_size", "1000"));
 		this.fileName = conf.getProperty("tgz_file_name", "temp");
+		this.separator = Integer.parseInt(conf.getProperty("tgz_row_separator", "7"));
 
 	}
 
@@ -52,15 +48,24 @@ public class ExecWork implements Runnable {
 			log.info("Write data into: " + this.fileName);
 			List row = null;
 			int n = 0;
-			JSONArray a = new JSONArray();
-
+			StringBuilder sb = new StringBuilder();
+			String tmp = null;
 			while (true) {
 				row = DataHolder.pop();
 				if (row != null) {
+					sb.setLength(0);
 					for (int i = 0; i < row.size(); i++) {
-						a.put(i, row.get(i) == null ? ns : row.get(i));
+						if (i > 0) {
+							sb.append((char) this.separator);
+						}
+						sb.append(row.get(i) == null ? "" : row.get(i));
+						tmp = row.get(i) + "";
+						if (tmp.contains((char) this.separator + "")) {
+							log.error("NM! You have perticular character ACII(" + this.separator + ").");
+							System.exit(0);
+						}
 					}
-					out.write((a.toString() + this.newline).getBytes());
+					out.write((sb.toString() + this.newline).getBytes());
 					n++;
 				} else if (DataHolder.isDone()) {
 					break;
